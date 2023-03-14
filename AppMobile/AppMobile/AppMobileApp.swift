89666a088b79db80447510439c421b8e47d20457
Weslie <weslie.rabeson@gmail.com>
2023-03-14 10:32:37 +0100

    init

---
+++ AppMobile/AppMobile/AppMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppMobileApp.swift
+//  AppMobile
+//
+//  Created by etud on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
