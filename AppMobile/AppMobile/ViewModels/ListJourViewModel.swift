e9ff197b20879756db5852e1f4425c99dab7fb22
Weslie <weslie.rabeson@gmail.com>
2023-03-24 10:54:25 +0100

    list modifs

--- AppMobile/AppMobile/ViewModels/ListJourViewModel.swift
+++ AppMobile/AppMobile/ViewModels/ListJourViewModel.swift
@@ -2,11 +2,44 @@
 import Foundation
 class ListJourViewModel: ObservableObject {
     
-    @Published var model : ListJourModel
+    @Published var jours = [JourViewModel]()
     
-    init(listjour: ListJourModel){
-        self.model = listjour
+    init(listjour: [JourViewModel]){
+        self.jours = listjour
     }
-        
+    func change(name: String) {
+        self.objectWillChange.send()
+    }
+    
+    func remove(atOffsets indexSet : IndexSet) {
+        self.jours.remove(atOffsets: indexSet)
+        self.objectWillChange.send()
+    }
+    
+    func move(fromOffsets indexSet : IndexSet, toOffset index: Int) {
+        self.jours.move(fromOffsets: indexSet, toOffset: index)
+        self.objectWillChange.send()
+    }
+    @Published var state : JourState = .ready {
+        didSet {
+            switch state {
+            case .loadingJours:
+                debugPrint("state loading UserVM")
+            case .loadedJours(let newJours):
+                //transformation UserDTO en UserViewModel
+                self.jours = newJours.map{ jour in JourViewModel(jour: jour)}
+                debugPrint("jai charge les donnees")
+                self.state = .ready
+            case .error:
+                debugPrint("error")
+                self.state = .ready
+            case .ready:
+                debugPrint("TrackViewModel: ready state")
+                debugPrint("--------------------------------------")
+            default:
+                break
+            }
+        }
+    }
     
 }
--- AppMobile/AppMobile/ViewModels/ListZoneViewModel.swift
+++ AppMobile/AppMobile/ViewModels/ListZoneViewModel.swift
@@ -2,11 +2,44 @@
 import Foundation
 class ListZoneViewModel: ObservableObject {
     
-    @Published var model : ListZoneModel
+    @Published var zones = [ZoneViewModel]()
     
-    init(listzone: ListZoneModel){
-        self.model = listzone
+    init(listzone: [ZoneViewModel]){
+        self.zones = listzone
     }
-        
+    func change(name: String) {
+        self.objectWillChange.send()
+    }
+    
+    func remove(atOffsets indexSet : IndexSet) {
+        self.zones.remove(atOffsets: indexSet)
+        self.objectWillChange.send()
+    }
+    
+    func move(fromOffsets indexSet : IndexSet, toOffset index: Int) {
+        self.zones.move(fromOffsets: indexSet, toOffset: index)
+        self.objectWillChange.send()
+    }
+    @Published var state : ZoneState = .ready {
+        didSet {
+            switch state {
+            case .loadingZones:
+                debugPrint("state loading UserVM")
+            case .loadedZones(let newZones):
+                //transformation UserDTO en UserViewModel
+                self.zones = newZones.map{ zone in ZoneViewModel(zone: zone)}
+                debugPrint("jai charge les donnees")
+                self.state = .ready
+            case .error:
+                debugPrint("error")
+                self.state = .ready
+            case .ready:
+                debugPrint("TrackViewModel: ready state")
+                debugPrint("--------------------------------------")
+            default:
+                break
+            }
+        }
+    }
     
 }
