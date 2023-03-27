8ec49ecd1389608e7d87f00a5a62d68373345b33
Weslie <weslie.rabeson@gmail.com>
2023-03-27 10:08:28 +0200

    Init all views

---
+++ AppMobile/AppMobile/Views/ListeBenevolesView.swift
@@ -0,0 +1,14 @@
+
+import SwiftUI
+
+struct ListeBenevolesView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct ListeBenevolesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListeBenevolesView()
+    }
+}
---
+++ AppMobile/AppMobile/Views/ListeCreneauxView.swift
@@ -0,0 +1,14 @@
+
+import SwiftUI
+
+struct ListeCreneauxView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct ListeCreneauxView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListeCreneauxView()
+    }
+}
