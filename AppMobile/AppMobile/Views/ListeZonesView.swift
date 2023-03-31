980b880f12ade2e891eee1329a48c43fd67172fa
Weslie <weslie.rabeson@gmail.com>
2023-03-31 16:33:05 +0200

    create zone

--- AppMobile/AppMobile/Views/ListeZonesView.swift
+++ AppMobile/AppMobile/Views/ListeZonesView.swift
@@ -4,6 +4,8 @@ import SwiftUI
 struct ListZonesView: View {
     @ObservedObject var zones : ListZoneViewModel
     var zoneIntent : ZoneIntent
+    @State var nomzone:String=""
+    @State var nbbenevole:Int=1
     init(viewModel : ListZoneViewModel){
         self.zones = viewModel
         self.zoneIntent = ZoneIntent(model: viewModel)
@@ -20,14 +22,30 @@ struct ListZonesView: View {
                 Divider()
                 Spacer()
                 VStack{
-                    List{
-                    
-                    }
+                    TextField("Nom de la zone",text :$nomzone)
+                                        .padding()
+                                        .cornerRadius(5.0)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 5)
+                                                .stroke(Color.blue, lineWidth: 1)
+                                        ).padding(.bottom,30)
+                                    
+                                    
+                                    TextField("Nombre de bénévoles nécessaires", value: $nbbenevole, formatter: NumberFormatter()).padding()
+                                        .cornerRadius(5.0)
+                                        .overlay(
+                                                RoundedRectangle(cornerRadius: 5)
+                                                    .stroke(Color.blue, lineWidth: 1)
+                                            ).padding(.bottom,30)
+
                 }
                 
-                Button("Ajouter Zone", action:{
+                Button("Ajouter Zone"){Task {
+                    await zoneIntent.createZone(zone: ZoneModel(_idzone: 0, nomzone: nomzone, nbbenevole: nbbenevole))}
                     
-                })
+                    
+                    
+                }
                 .padding()
                 .background(vert_nav)
                 .cornerRadius(20)
@@ -50,6 +68,7 @@ struct ListZonesView: View {
             .task {
                 debugPrint("chargement data ?")
                     await zoneIntent.getZones()
+                    
             }
             }
             
