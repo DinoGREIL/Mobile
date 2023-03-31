8ceef3f1daf9e073b1a8deba55735cfe875b66db
laulilaura <laurabenaiton@gmail.com>
2023-03-31 15:46:39 +0200

    Accueil

--- AppMobile/AppMobile/ContentView.swift
+++ AppMobile/AppMobile/ContentView.swift
@@ -13,75 +13,35 @@ struct ContentView: View {
     @ObservedObject var festivals = ListFestivalViewModel(listfestival: [])
     
     var body: some View {
-        TabView {
+        NavigationStack{
             VStack {
                 Text("Accueil")
                     .font(.system(size: 25))
                     .padding()
                 Divider()
                 Spacer()
-                // Bleeds into TabView
-                Rectangle()
-                    .frame(height: 3)
-                    .background(vert_nav)
-                }
-            .tabItem{
-                Label("Accueil",systemImage: "house.fill")
             }
-                .background(beige_fond)
-            
-            VStack {
-                Text("Gestion des Jeux")
-                    .font(.system(size: 25))
-                    .padding()
-                Divider()
+            .background(beige_fond)
+            VStack{
+                NavigationLink("Gestion des festivals"){
+                    FestivalsView(viewModel: ListFestivalViewModel(listfestival: []))
+                }
+                NavigationLink("Gestion des Jours"){
+                    ListeJoursView(viewModel: ListJourViewModel(listjour: []),viewModel2: ListFestivalViewModel(listfestival: []))
+                }
+                NavigationLink("Gestion des Creneaux"){
+                    ListeCreneauxView(viewModel: ListCreneauViewModel(listcreneau: []),viewmodel2:ListJourViewModel(listjour: []))
+                }
+                NavigationLink("Gestion des Bénévoles"){
+                    ListeBenevolesView(viewModel: ListBenevolesViewModel(benevoles: []),viewmodel2: ListDisponibleViewModel(list: []))
+                }
+                NavigationLink("Gestion des Zones"){
+                    ListZonesView(viewModel: ListZoneViewModel(listzone: []))
+                }
                 Spacer()
-                // Bleeds into TabView
-                Rectangle()
-                        .frame(height: 3)
-                        .background(vert_nav)
-                }
-                .tabItem {
-                    Label("Jeux",systemImage: "checkerboard.rectangle")
-                }
-                .background(beige_fond)
-            
-            VStack {
-                Text("Gestion des Bénévoles")
-                    .font(.system(size: 25))
-                    .padding()
-                Divider()
-                Spacer()
-                // Bleeds into TabView
-                Rectangle()
-                        .frame(height: 3)
-                        .background(vert_nav)
-                }
-                .tabItem {
-                    Label("Bénévoles",systemImage: "person.fill")
-                }
-                .background(beige_fond)
-            
-            VStack {
-                Text("Gestion des Zones")
-                    .font(.system(size: 25))
-                    .padding()
-                Divider()
-                Spacer()
-                // Bleeds into TabView
-                Rectangle()
-                    .frame(height: 3)
-                    .background(vert_nav)
-                
-                }
-                .tabItem {
-                    Label("Zones",systemImage:"mappin.and.ellipse")
-                }
-                .background(beige_fond)
-            
-            
-            
+            }
         }
+        
         ZStack {
             beige_fond
                 .ignoresSafeArea()
